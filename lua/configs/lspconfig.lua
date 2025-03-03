adabcfefac720a5ed0997c3641c176e3c8e7dfd7
JianZcar <esteban.jianzcar@outlook.com>
2025-03-03 14:09:47 +0800

    added lsp python, php, tailwind, js

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,8 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "pylsp", "intelephense", "html", "cssls", "tailwindcss", "ts_ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
