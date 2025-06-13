6d2cf46f447fb3cf4b2346292093585f08d46ad5
JianZcar <esteban.jianzcar@outlook.com>
2025-06-14 01:49:14 +0800

    no more mouse, intelephense wont polute my home dir

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,11 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "bashls", "pylsp", "intelephense", "html", "cssls", "tailwindcss", "ts_ls" }
+vim.lsp.config("intelephense", {
+  init_options = {
+    globalStoragePath = os.getenv("HOME") .. "/.local/share/intelephense",
+  },
+})
+
+local servers = { "bashls", "pylsp", "html", "intelephense", "cssls", "tailwindcss", "ts_ls"}
 vim.lsp.enable(servers)
